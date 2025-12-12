c76c2a0ec69a341d3879982dc8089ad6a42d74f4
Sarah Schaech <ses501@pitt.edu>
2025-12-12 11:11:11 -0500

    Added move-sound-files shell script

---
+++ move-sound-files.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# unzip and move audio to sound-files folder
+# make sure you run this in the terminal as: bash move-sound-files.sh
+
+unzip -o "Training Stimuli 2-Talker Group".zip -d sound-files/
+
+unzip -o "Training Stimuli 6-Talker Group".zip -d sound-files/
+
+
+
